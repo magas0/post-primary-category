8257e8525e183c7a6268dbcaa823bd65f0af1f34
Magas <magas0@users.noreply.github.com>
2017-11-10 10:34:00 -0500

    Removed some comments

--- uninstall.php
+++ uninstall.php
@@ -1,13 +1,4 @@
 <?php
-/*
-  Plugin Name: Post Primary Category (10up)
-  Plugin URI: https://github.com/magas0/primary-category-10up
-  Description: a plugin to allow publishers designate a primary category for posts.
-  Version: 0.1
-  Author: SM
-  Author URI: https://github.com/magas0/
-  License: GPL2
-*/
 
 // if uninstall.php is not called by WordPress, die
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
